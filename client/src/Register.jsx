1798513eda1328214d304b8ec5c9795d93d9cff1
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-01 23:13:37 +0300

    Updated Register

--- client/src/Register.jsx
+++ client/src/Register.jsx
@@ -1,12 +1,16 @@
 import axios from "axios"
-import { useState } from "react"
+import { useContext, useState } from "react"
+import { UserContext } from "./UserContext"
 
 export default function Register() {
-    const [username, setUsername] = useState()
-    const [password, setPassword] = useState()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const {setUsername:setLoggedInUsername, setId} = useContext(UserContext)
     async function register(e) {
         e.preventDefault()
-        await axios.post('/register', {username,password})
+        const {data} = await axios.post('/register', {username,password})
+        setLoggedInUsername(username)
+        setId(data.id)
     }
     return (
         <div className="bg-blue-50 h-screen flex items-center">
