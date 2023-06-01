fcddcacc7eded1761a65b8bb261794a45ec68de7
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-01 23:12:03 +0300

    Added Routes

---
+++ client/src/Routes.jsx
@@ -0,0 +1,14 @@
+import { useContext } from "react";
+import Register from "./Register";
+import { UserContext } from "./UserContext";
+
+export default function Routes() {
+    const {username, id} = useContext(UserContext)
+
+    if (username) {
+        return 'logged in!'
+    }
+    return (
+        <Register />
+    )
+}
