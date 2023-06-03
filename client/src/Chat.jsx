e027a9b88a69b833df59209623aadf4711103a92
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-06-03 12:11:04 +0300

    Updated Chat

--- client/src/Chat.jsx
+++ client/src/Chat.jsx
@@ -1,31 +1,84 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import Avatar from "./Avatar"
+import Logo from "./Logo"
+import { UserContext } from "./UserContext"
 
 export default function Chat() {
     const [ws,setWs] = useState(null)
+    const [onlinePeople, setOnlinePeople] = useState({})
+    const [selectedUserId, setSelectedUserId] = useState(null)
+    const [newMessageText,setNewMessageText] = useState('')
+    const {username, id} = useContext(UserContext)
     useEffect(() => {
         const ws = new WebSocket('ws://localhost:4000')
         setWs(ws)
         ws.addEventListener('message', handleMessage)
     }, [])
-    function handleMessage(e) {
-        e.data.text().then(messageString)
+    function showOnlinePeople(peopleArray) {
+        const people = {}
+        peopleArray.forEach(({userId,username}) => {
+            people[userId] = username
+        })
+        setOnlinePeople(people)
     }
+    function handleMessage(e) {
+        const messageData = JSON.parse(e.data)
+        if ('online' in messageData) {
+            showOnlinePeople(messageData.online)
+        }
+    }
+    function sendMessage(e) {
+        e.preventDefault()
+        ws.send(JSON.stringify({
+            message: {
+                recipient: selectedUserId,
+                text: newMessageText,
+            }
+        }))
+    }
+    const onlinePeopleExclOurUser = {...onlinePeople}
+    delete onlinePeopleExclOurUser[id]
+    
+    
     return (
         <div className="flex h-screen">
-            <div className="bg-white w-1/3">
-                Contacts
+            <div className="bg-white w-1/3"> 
+                <Logo />
+
+                {Object.keys(onlinePeople).map(userId => (
+                    <div key={userId} onClick={() => setSelectedUserId(userId)}
+                    className={"border-b border-gray-100 flex items-center gap-2 cursor-pointer"+ 
+                    (userId === selectedUserId ? 'bg-blue-50' : '')}>
+                        {userId === selectedUserId && (
+                            <div className="w-1 bg-blue-500 h-12 rounded-r-md"></div>
+                        )}
+                        <div className="flex gap-2 py-2 pl-4 items-center">
+                        <Avatar username={onlinePeople[userId]} userId={userId} />
+                        <span className="text-gray-800">{onlinePeople[userId]}</span>
+                        </div>
+                        </div>
+                ))}
                 </div>
             <div className="flex flex-col bg-blue-100 w-2/3 p-2">
-                <div className="flex-grow">Messages with selected person</div>
-                <div className="flex gap-2">
-                    <input className="bg-white flex-grow border p-2 rounded-sm" type="text" 
+                <div className="flex-grow">
+                    {!selectedUserId && (
+                        <div className="flex h-full flex-grow items-center justify-center">
+                            <div className="text-gray-300">&larr; Select a person from the sidebar</div>
+                        </div>
+                    )}
+                </div>
+                {!!selectedUserId && (
+                    <form className="flex gap-2" onSubmit={sendMessage}>
+                    <input value={newMessageText} onChange={e => setNewMessageText(e.target.value)} 
+                    className="bg-white flex-grow border p-2 rounded-sm" type="text" 
                     placeholder="Type youe message here" />
-                    <button className="bg-blue-500 p-2 text-white rounded-sm">
+                    <button type="submit" className="bg-blue-500 p-2 text-white rounded-sm">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
                         </svg>
                     </button>
-                </div>
+                </form>  
+                )}
             </div>
         </div>
     )
